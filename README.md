746a9a0d20acbc3ec978c18d962bb2e6a15ba63c
TyLaneTech <61366689+TyLaneTech@users.noreply.github.com>
2020-05-05 17:22:04 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,101 @@
+I. OVERVIEW
+
+A state machine is a device or program that can store the status of something at a certain time. Based on input the status can change, providing the resulting output. 
+State machines begin from what's called an initial state. A transition will take place based on provided inputs. Based on these inputs the user will move to other states which will also have transition functions; until an end state is reached
+
+
+
+II. HOW DOES IT WORK?
+
+First we need to define a dictionary called 'handlers' which will hold the various states, and references to their associated transition functions. Next we define a variable called 'startState' which will indicate the initial state of the machine. Finally we define a list object called 'endStates' which contains the names of the various end states. Shown below:
+
+											   ###Containers###
+
+		handlers = {}
+		startState = None
+		endStates = []
+
+
+Next we need to call the 'addState' function which takes the following arguements: The name of the state, that state's transition function, and if the state is an end state.
+Additionally the 'setStart' function sets the 'startState' variable to the name of the defined starting state (in this case "Start"). Shown below:
+
+										  ###Creating State Instances###
+
+		addState("Start", startTransition)
+		addState("isState", isStateTransition)
+		addState("positiveState", None, endState=True)
+		setStart("Start")
+
+
+Below are the 'addState' and 'setStart' functions, which are called, and given arguments from the lines above:
+
+										   ###State Creation Methods###
+
+		def addState(self, name, handler, endState=False):
+			name = name.upper()
+			self.handlers[name] = handler
+			if endState:
+				self.endStates.append(name)
+				
+		def setStart(self, name):
+			self.startState = name.upper()
+		
+		
+To run the state machine created above, the 'run' method must be called (shown below). The 'sentence' argument being passed contains the phrase "AI is" concatenated with the user's input (for this example we'll assume the user's input is "neat"). So the full sentence passed to the 'run' method is: "AI is neat".
+	
+										 ###Calling The 'run' Method###
+										
+		run(sentence)
+
+
+The 'run' method (shown below) takes the sentence described above as an argument. From this point on, the sentence above will be refered to as 'userInput'.
+In line 2 of the 'run' function below, the function associated with the 'startState' is called ('startTransition'), and the 'userInput' is passed as an argument. 
+
+											 ###The 'run' Method###
+												
+1		def run(self, userInput):
+2			handler = self.handlers[self.startState]
+3			while True:
+4				(newState, userInput) = handler(userInput)
+5				if newState.upper() in self.endStates:
+6					if newState == "errorState":
+7						print("Arrived at", newState, "(Unknown Adjective)")
+8					if newState != "errorState":
+9						print("Arrived at", newState)
+10					break 
+11				else:
+12					handler = self.handlers[newState.upper()]
+
+
+At this point the user input is "AI is neat". Inside the transition function below the first word of 'userInput' is evaluated ("AI"), and based on the evaluated word the new state's name ('aiState') is assigned to the variable 'newState'. 
+The remainder of the user input ("is neat"), along with the 'newState' variable ('aiState') are returned back to the 'run' method as 'newState' and 'txt'. 
+		
+											###Transition Function###
+											
+		def startTransition(txt):
+			splitText = txt.split(None,1)
+			word, txt = splitText if len(splitText) > 1 else (txt,"")
+			
+			if word == "AI":
+				newState = 'aiState'
+			else:
+				newState = 'errorState'
+			return (newState, txt)
+			
+			
+Once 'newState' and 'txt' are returned to the 'run' method, the transition function associated with 'aiState' is called (line 4 of the 'run' method), and the user input "is neat" (returned from the transition function above) is passed to the next transition function ('aiStateTransition') where the process continues to repeat until all the words in the user's input are evaluated, or an end state is reached.
+
+
+
+III. HOW IS THIS DATA STRUCTURE USEFUL?
+
+State machines are used in many scientific applications; like modeling the behavior of applications, designing digital systems, software engineering, compilers, network protocols, and much more. The idea of going from one state to another based on some input is a foundation of modern computing.
+
+
+
+IV. FURTHER READING
+
+https://www.techopedia.com/definition/16447/state-machinehttps://www.bogotobogo.com/python/python_graph_data_structures.php
+https://www.python-course.eu/finite_state_machine.php
+https://www.geeksforgeeks.org/state-design-pattern/
+https://en.wikipedia.org/wiki/Finite-state_machine
---
+++ statemachine.py
@@ -0,0 +1,179 @@
+#The functions below display aditional process information if the user requests.
+firstUse = 0
+info = False
+def endStateInfo(word, newState):
+	if info == True: print("The function above evaluated the word '"+ word + "'.  Arrived at: '" + newState +"'. Which is an end state\n")
+	else: pass
+def regStateInfo(word, newState, txt):
+	if info == True: print("The function above evaluated the word '"+ word + "'.  Arrived at: '" + newState +"'.  Passing: '" + txt + "' to the function below:")
+	else: pass
+	
+	
+
+class StateMachine:
+	def __init__(self):
+		self.handlers = {} #Contains the names of the various states, and the transition functions associated with them.
+		self.startState = None #Contains the start state.
+		self.endStates = [] #Contains the possible end states.
+		
+#Function for adding a state & it's transition function to the 'handlers' dictionary. Takes the name, transition function, and whether or not the declared state is an end state as arguments.
+	def addState(self, name, handler, endState=False):
+		name = name.upper()
+		self.handlers[name] = handler
+		if endState:
+			self.endStates.append(name)
+
+#Function for defining the start state.
+	def setStart(self, name):
+		self.startState = name.upper()
+
+#Function that initiates the state traversal algorithm (line 32). Also takes arguments from transition functions, and calls the next function based on those arguments (line 34). Once a state defined as an end state is reached it prints the final state, and breaks the loop.
+	def run(self, userInput):
+		handler = self.handlers[self.startState]
+		while True:
+			(newState, userInput) = handler(userInput)
+			if newState.upper() in self.endStates:
+				if newState == "errorState":
+					print("Arrived at", newState, "(Unknown Adjective)")
+				if newState != "errorState":
+					print("Arrived at", newState)
+				break 
+			else:
+				handler = self.handlers[newState.upper()]
+				if info == True: print(handler)
+
+
+
+#Adjective lists
+positiveAdjectives = ["clean", "good", "great", "entertaining", "sweet", "nice", "dope", "terrific", "fun", "cool", "neat", "intriguing", "positive"]
+negativeAdjectives = ["poop", "hard", "ugly", "bad", "difficult", "awful", "terrible", "gross", "foolish", "dumb", "negative", "stupid", "scary", "troubling", "dangerous"]
+
+#Holds names of the states that have been visited to display to the user once the run completes.
+statesTraversed = ""
+
+
+#The function below parses the sentence "...AI is (userInput)", if the word "AI" is present, it will return the 'aiState' and pass the rest of the sentence to the 'aiStateTransition' function. If "AI" is not present, it will return the 'errorState'.
+def startTransition(txt):
+	global statesTraversed
+	splitText = txt.split(None,1)
+	word, txt = splitText if len(splitText) > 1 else (txt,"")
+	
+	if word == "AI":
+		newState = "aiState"
+		statesTraversed = statesTraversed + (newState) + " -> "
+		if info == True:
+			print("The 'startTransition' function evaluated the word '"+ word + "'.  Arrived at: '" + newState +"'. Passing: '" + txt + "' to the function below:")	
+	else:
+		newState = "errorState"
+		statesTraversed = statesTraversed + (newState) + " (End State)"
+		endStateInfo(word, newState)
+	return (newState, txt)
+
+
+#The function below parses the rest of the sentence, if the word "is" is present, it will return the 'isState', and pass the rest of the sentence to the 'isStateTransition' function. If "is" is not present, it will return the 'errorState'.
+def aiStateTransition(txt):
+	global statesTraversed
+	splitText = txt.split(None,1)
+	word, txt = splitText if len(splitText) > 1 else (txt,"")
+	
+	if word == "is":
+		newState = "isState"
+		statesTraversed = statesTraversed + (newState) + " -> "
+		regStateInfo(word, newState, txt)
+	else:
+		newState = "errorState"
+		statesTraversed = statesTraversed + (newState) + " (End State)"
+		endStateInfo(word, newState)
+	return (newState, txt)
+
+
+#The function below parses the rest of the sentence; if the word "not" is present, it will return the notState, and pass the rest of the sentence (which should be the last word) to the 'notStateTransition'. If the word "not" is not present it will return either the 'positiveState' or the 'negativeState' depending on whether the next word is in the 'positiveAdjectives' or 'negativeAdjectives' lists. If the next word isnt in either list, it will return the 'errorState'.
+def isStateTransition(txt):
+	global statesTraversed
+	splitText = txt.split(None,1)
+	word, txt = splitText if len(splitText) > 1 else (txt,"")
+	
+	if word == "not":
+		newState = "notState"
+		statesTraversed = statesTraversed + (newState) + " -> "
+		regStateInfo(word, newState, txt)
+	elif word in positiveAdjectives:
+		newState = "positiveState"
+		statesTraversed = statesTraversed + (newState) + " (End State)"
+		endStateInfo(word, newState)
+	elif word in negativeAdjectives:
+		newState = "negativeState"
+		statesTraversed = statesTraversed + (newState) + " (End State)"
+		endStateInfo(word, newState)
+	else:
+		newState = "errorState"
+		statesTraversed = statesTraversed + (newState) + " (End State)"
+		endStateInfo(word, newState)
+	return (newState, txt)
+
+
+#Since this function is performed after the word "not" is detected it will return the 'negativeState' if the next word is present in the 'positiveAdjectives' list; or the 'positiveState' if the next word is present in the 'negativeAdjectives' list. If the user's input is not present, it will return the 'errorState'.
+def notStateTransition(txt):
+	global statesTraversed
+	splitText = txt.split(None,1)
+	word, txt = splitText if len(splitText) > 1 else (txt,"")
+	
+	if word in positiveAdjectives:
+		newState = "negativeState"
+		statesTraversed = statesTraversed + (newState) + " (End State)"
+	elif word in negativeAdjectives:
+		newState = "positiveState"
+		statesTraversed = statesTraversed + (newState) + " (End State)"
+	else:
+		newState = "errorState"
+		statesTraversed = statesTraversed + (newState) + " (End State)"
+	endStateInfo(word, newState)
+	return (newState, txt)
+
+
+
+if __name__== "__main__":
+	m = StateMachine()
+	m.addState("Start", startTransition)				#Declare state, and transition function
+	m.addState("aiState", aiStateTransition)			#Declare state, and transition function
+	m.addState("isState", isStateTransition)			#Declare state, and transition function
+	m.addState("notState", notStateTransition)			#Declare state, and transition function
+	m.addState("negativeState", None, endState=True)	#Declare end state
+	m.addState("positiveState", None, endState=True)	#Declare end state
+	m.addState("errorState", None, endState=True)		#Declare end state
+	m.setStart("Start")									#Declare the start state
+	
+	print("Enter a positive or negative adjective that completes the following sentence:")
+	print("\nFor Example:")
+	print("Positive: 'great' 'neat' 'not bad' 'not scary' \nNegative: 'bad' 'scary' 'not great' 'not neat'\n")
+
+#User choice for process information	
+	while True:
+		if firstUse == 1:
+			moreInfo = input("Would you like to see more detail on the processes at work? Y/N: ")
+			if moreInfo == 'Y'or moreInfo == 'y':
+				info = True
+			else:
+				pass
+				
+#User prompt without examples
+		if firstUse != 0:
+			statesTraversed = ""
+			print("\n \nEnter a positive or negative adjective that completes the following sentence:")
+		firstUse = firstUse + 1
+		usrInput = input("I think AI is ")
+		print("")
+		if info == True: print("Information:")
+		sentence = "AI is " + usrInput
+
+#Calls the function that initiates the state traversal sequence, with the sentence containing the user's input.
+		m.run(str(sentence))	
+
+#Prints the names of the states that were visited during the traversal process.
+		print("States traversed: "+ str(statesTraversed))
+				
+#Try again prompt
+		res = input("\nTry again? Y/N: ")
+		if res == 'N'or res == 'n':
+			print("\nBye!")
+			break
